9f5ad331897ad966dc157cc4ca6b19b13f1da690
ialsghi123 <ialsghi123@naver.com>
2020-04-20 22:56:21 +0900

    20200420

--- a001_hello/src/main/java/com/lec/android/androidwork/MainActivity.java
+++ a001_hello/src/main/java/com/lec/android/androidwork/MainActivity.java
@@ -2,13 +2,49 @@ package com.lec.android.androidwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Dialog dialog;
+    Button btn_ok,btn_no;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.my_layout);
     }
+
+    @Override
+    public void onBackPressed() {
+        dialog=new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.my_layout);
+
+        btn_ok=dialog.findViewById(R.id.btn_yes);
+        btn_no=dialog.findViewById(R.id.btn_no);
+
+        btn_ok.setOnClickListener(click);
+        btn_no.setOnClickListener(click);
+
+        dialog.setTitle("app name");
+        dialog.show();
+    }
+
+    View.OnClickListener click = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_yes:
+                    finish();
+                    break;
+
+                case R.id.btn_no:
+                    dialog.dismiss();
+                    break;
+            }
+        }
+    };
 }
