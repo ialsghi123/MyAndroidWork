b10dea51c1748fae87c0d981a7f7ca4df7c9fbbc
ialsghi123 <ialsghi123@naver.com>
2020-04-21 22:53:07 +0900

    20200421

--- a001_hello/src/main/java/com/lec/android/androidwork/MainActivity.java
+++ a001_hello/src/main/java/com/lec/android/androidwork/MainActivity.java
@@ -17,34 +17,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_layout);
     }
-
-    @Override
-    public void onBackPressed() {
-        dialog=new Dialog(MainActivity.this);
-        dialog.setContentView(R.layout.my_layout);
-
-        btn_ok=dialog.findViewById(R.id.btn_yes);
-        btn_no=dialog.findViewById(R.id.btn_no);
-
-        btn_ok.setOnClickListener(click);
-        btn_no.setOnClickListener(click);
-
-        dialog.setTitle("app name");
-        dialog.show();
-    }
-
-    View.OnClickListener click = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()){
-                case R.id.btn_yes:
-                    finish();
-                    break;
-
-                case R.id.btn_no:
-                    dialog.dismiss();
-                    break;
-            }
-        }
-    };
+//
+//    @Override
+//    public void onBackPressed() {
+//        dialog=new Dialog(MainActivity.this);
+//        dialog.setContentView(R.layout.my_layout);
+//
+//        btn_ok=dialog.findViewById(R.id.btn_yes);
+//        btn_no=dialog.findViewById(R.id.btn_no);
+//
+//        btn_ok.setOnClickListener(click);
+//        btn_no.setOnClickListener(click);
+//
+//        dialog.setTitle("app name");
+//        dialog.show();
+//    }
+//
+//    View.OnClickListener click = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            switch (v.getId()){
+//                case R.id.btn_yes:
+//                    finish();
+//                    break;
+//
+//                case R.id.btn_no:
+//                    dialog.dismiss();
+//                    break;
+//            }
+//        }
+//    };
 }
