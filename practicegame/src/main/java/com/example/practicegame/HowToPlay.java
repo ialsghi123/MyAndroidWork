0df90e74f66d3cba86c87cc66c28a9f8585f9c24
ialsghi123 <ialsghi123@naver.com>
2020-04-22 09:19:33 +0900

    20200422

--- practicegame/src/main/java/com/example/practicegame/HowToPlay.java
+++ practicegame/src/main/java/com/example/practicegame/HowToPlay.java
@@ -10,5 +10,6 @@ public class HowToPlay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.howtoplay);
+        //
     }
 }
